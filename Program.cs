a2542ba70f2c31447f7a98fc71c47a351f784faf
Kennedy Mungai <kennedymungaifmab@gmail.com>
2022-07-15 09:08:09 +0300

    Created the GetSetup method for initial data

--- Program.cs
+++ Program.cs
@@ -10,4 +10,17 @@ public class Program
         ExcelPackage.LicenseContext = LicenseContext.NonCommercial;
         var file = new FileInfo(@"C:\Excel\ExcelDemo.xlsx");
     }
+
+
+    static List<PersonModel> GetSetupData()
+    {
+        List<PersonModel> output = new()
+        {
+            new() {Id =1, FirstName="Kill", LastName="Monger"},
+            new() {Id =2, FirstName="Storm", LastName="Front"},
+            new() {Id =3, FirstName="Bear", LastName="Hug"}
+        };
+
+        return output;
+    }
 }
