9de3b9b42152431fa6f6745187adc31a9c5b32c9
Kennedy Mungai <kennedymungaifmab@gmail.com>
2022-07-15 08:57:55 +0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using OfficeOpenXml;
+using System.IO;
+
+namespace ExcelDemoApp;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        ExcelPackage.LicenseContext = LicenseContext.NonCommercial;
+        var file = new FileInfo(@"");
+    }
+}
